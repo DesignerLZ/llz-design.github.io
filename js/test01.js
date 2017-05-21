ae8c7dfa7891a2f0d3a9faf66f95bfd475087f01
DesignerLZ <563242813@qq.com>
2017-05-22 01:17:20 +0800

    Construction-2017.5.21
    建设中，开始建设照片集区域

--- js/test01.js
+++ js/test01.js
@@ -1,33 +1,44 @@
-$.fn.toggle = function( fn, fn2 ) {
-    var args = arguments,guid = fn.guid || $.guid++,i=0,
-    toggle = function( event ) {
-      var lastToggle = ( $._data( this, "lastToggle" + fn.guid ) || 0 ) % i;
-      $._data( this, "lastToggle" + fn.guid, lastToggle + 1 );
-      event.preventDefault();
-      return args[ lastToggle ].apply( this, arguments ) || false;
-    };
-    toggle.guid = guid;
-    while ( i < args.length ) {
-      args[ i++ ].guid = guid;
-    }
-    return this.click( toggle );
-  };
+function Showup00(){
+     $(".welcome00").css("opacity","1");
+}
+function Showup01(){
+     $(".welcome00").css("opacity","0");
+     $(".welcome01").delay(200).animate({opacity:"1"});
+}
+function Showup02(){
+     $(".welcome01").css("opacity","0");
+     $(".welcome02").delay(200).animate({opacity:"1"});
+}
+function Showup03(){
+     $(".welcome02").css("opacity","0");
+     $(".welcome03").delay(200).animate({opacity:"1"});
+}
 $(document).ready(function(e){
-$("#newscontrol").toggle(
-  function(){
-      $("#newsbox01").css("height","0");
-      $("#newsbox01").css("margin-top","0");
-      $("#newsbox02").css("height","0");
-      $("#navlistleft").css("top","-35%");
-      $("#up").css("top","-135px");},
-  function(){
-      $("#newsbox01").css("height","40%");
-      $("#newsbox01").css("margin-top","3%");
-      $("#newsbox02").css("height","40%");
-      $("#navlistleft").css("top","0");
-      $("#up").css("top","-55px");},
-  function(){
+ setTimeout("Showup00()",1000);
+ setTimeout("Showup01()",4000);
+ setTimeout("Showup02()",7000);
+ setTimeout("Showup03()",10000);
+    var i=0;
+  $("#newscontrol").on("click",function(){
+if (i===0)
+  {
+  $("#newsbox01").css("height","40%");
+  $("#newsbox01").css("margin-top","3%");
+  $("#newsbox02").css("height","40%");
+  $("#navlistleft").css("top","0");
+  $("#up").css("top","-45px");
+  i=1;
   }
-);
+else if(i===1)
+  {
+  $("#newsbox01").css("height","0");
+  $("#newsbox01").css("margin-top","0");
+  $("#newsbox02").css("height","0");
+  $("#navlistleft").css("top","-35%");
+  $("#up").css("top","-135px");
+  i=0;
+  }
+  })
+
 })
 
